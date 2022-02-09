bd9645d09d69ca3f1568e489ad1818cdfca56948
Sad Pencil <qh06@qq.com>
2022-02-09 17:01:08 +0800

    Bump version

--- constant.py
+++ constant.py
@@ -1,6 +1,6 @@
 NAME = 'github-release-downloader'
 OWNER = 'SDU-Mirrors'
 FULL_NAME = '{}/{}'.format(OWNER, NAME)
-VERSION = 'v0.1'
+VERSION = 'v0.1.1'
 UA_NAME = '{} {}'.format(NAME, VERSION)
 REPO_URL = 'https://github.com/SDU-Mirrors/github-release-downloader'
